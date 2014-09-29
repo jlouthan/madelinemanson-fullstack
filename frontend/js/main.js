b8f6aec5c7e8b7ad1f8bded14334b6cb8f57f057
Jennifer Louthan <jenny@uncorkedstudios.com>
2014-09-28 20:26:07 -0700

    carousel thing working

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,25 +6,50 @@ $(window).load(function(){
 		}
 	});
 
+	if($('#carousel').data('total-pics') == 1){
+		$("#right-arrow").css('visibility', 'hidden');
+	}
+
 	$("#right-arrow").click(function(){
+		var arrow = $(this);
 		var i = $('#carousel').data('visible-index');
 		var total = $('#carousel').data('total-pics');
-		console.log(total);
-		if(i == total){
-			var nextI = 1;
-			console.log('in this loop!');
-		}
-		else{
-			var nextI = i+1;
-		}
-		console.log('nexti is ' + nextI);
+		var nextI = i + 1;
 		var showing = $( "#carousel li:nth-child(" + i + ")" );
 		var next = $( "#carousel li:nth-child(" + nextI + ")" )
 		showing.fadeOut("fast", function(){
-			$('#carousel').attr('data-visible-index', nextI);
+			if(nextI == total){
+				arrow.css('visibility', 'hidden');
+			}
+			if(nextI == 2){
+				$('#left-arrow').css('visibility', 'visible');
+			}
+			$('#carousel').data('visible-index', nextI);
 			next.fadeIn('fast', function(){
 
 			});
 		});
 	});
+
+	$("#left-arrow").click(function(){
+		var arrow = $(this);
+		var i = $('#carousel').data('visible-index');
+		var total = $('#carousel').data('total-pics');
+		var prevI = i - 1;
+		var showing = $("#carousel li:nth-child(" + i + ")");
+		var prev = $("#carousel li:nth-child(" + prevI + ")");
+		showing.fadeOut("fast", function(){
+			if(prevI == total-1){
+				$('#right-arrow').css('visibility', 'visible');
+			}
+			if(prevI == 1){
+				arrow.css('visibility', 'hidden');
+			}
+			$("#carousel").data('visible-index', prevI);
+			prev.fadeIn("fast", function(){
+
+			});
+		});
+	});
+
 });
