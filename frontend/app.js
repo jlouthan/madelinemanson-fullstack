112e9c76205734c73e150a8172d12fdb6f7b883f
Jennifer Louthan <jenny@uncorkedstudios.com>
2015-01-26 22:02:09 -0800

    frontend updates

--- frontend/app.js
+++ frontend/app.js
@@ -45,6 +45,14 @@ var hbs = exphbs.create({
             if (param1 + param2 != param3) {
                 return options.fn(this);
             }
+        },
+        // Returns 2 HTML data attributes in the format 'data-src="[url]" data-src-retina="[url]"' to be used in conjunction with unveil.js
+        retinaImg: function(param1, param2) {
+            imageData = 'data-src="' + param1 + '" data-src-retina="' + param2 + '"';
+            return(imageData);
+        },
+        toLowerCase: function(str) {
+            return str.toLowerCase();
         }
 	}
 });
@@ -87,6 +95,15 @@ var requestHandler = function(req, res, pageView, pageTitle, bodyClass, paths){
             feed.setItem = feed.object;
             delete feed.object;
         }
+        if(pageView === 'set-prints'){
+            // find the index of the print by slug
+            for(var i = 0; i < feed.prints.length; i++){
+                if(feed.prints[i].slug === bodyClass){
+                    bodyClass = i+1;
+                    break;
+                }
+            }
+        }
         console.log(feed);
         res.render(pageView,{
             title: pageTitle,
@@ -117,66 +134,67 @@ var requestHandler = function(req, res, pageView, pageTitle, bodyClass, paths){
 };
 
 app.get('/', function (req, res) {
-	res.render('index');
-});
-
-app.get('/home', function (req, res) {
-    res.render('home');
+    var path = 'etc';
+    requestHandler(req, res, 'index', 'Home', 'home', [path]);
 });
 
 app.get('/prints', function (req, res) {
-	res.render('collection', {title: 'prints'});
+    var path = 'prints?{"state":"published","$sort":{"weighting":-1}}';
+    requestHandler(req, res, 'list-set-prints', 'Prints', 'prints', [path]);
 });
 
 app.get('/prints/:printName', function(req, res){
-	res.render('set');
+    var slug = req.params.printName;
+    var path = 'prints?{"state":"published","$sort":{"weighting":-1}}';
+    requestHandler(req, res, 'set-prints', 'Prints', slug, [path]);
 });
 
 app.get('/textiles', function (req, res) {
-    var path = 'textiles?state=published';
-    requestHandler(req, res, 'collection', 'Madi Manson - Textiles', 'textiles', [path]);
-	// res.render('collection', {title: 'textiles'});
+    var path = 'textiles?{"state":"published","$sort":{"weighting":-1}}';
+    requestHandler(req, res, 'collection', 'Textiles', 'textiles', [path]);
 });
 
 app.get('/textiles/:textileName/:textileId', function (req, res){
     var slug = req.params.textileName;
     var imageIndex = req.params.textileId;
     var path = 'textiles?state=published&slug=' + slug;
-    requestHandler(req, res, 'set', 'Madi Manson - Textiles', imageIndex, [path]);
+    requestHandler(req, res, 'set', 'Textiles', imageIndex, [path]);
 });
 
 app.get('/textiles/:textileName', function (req, res){
     var slug = req.params.textileName;
     var path = 'textiles?state=published&slug=' + slug;
-    requestHandler(req, res, 'list-set', 'Madi Manson - Textiles', 'textiles', [path]);
+    requestHandler(req, res, 'list-set', 'Textiles', 'textiles', [path]);
 });
 
 app.get('/objects', function (req, res) {
-    var path = 'objects?state=published';
-    requestHandler(req, res, 'collection', 'Madi Manson - Objects', 'objects', [path]);
+    var path = 'objects?{"state":"published","$sort":{"weighting":-1}}';
+    requestHandler(req, res, 'collection', 'Objects', 'objects', [path]);
 });
 
 app.get('/objects/:objectName/:objectId', function (req, res){
     var slug = req.params.objectName;
     var imageIndex = req.params.objectId;
     var path = 'objects?state=published&slug=' + slug;
-    requestHandler(req, res, 'set', 'Madi Manson - Objects', imageIndex, [path]);
+    requestHandler(req, res, 'set', 'Objects', imageIndex, [path]);
 });
 
 app.get('/objects/:objectName', function (req, res){
     var slug = req.params.objectName;
     var path = 'objects?state=published&slug=' + slug;
-    requestHandler(req, res, 'list-set', 'Madi Manson - Objects', 'objects', [path]);
+    requestHandler(req, res, 'list-set', 'Objects', 'objects', [path]);
 });
 
 app.get('/about', function (req, res){
-	var path = 'cv';
-    requestHandler(req, res, 'about', 'Madi Manson - About', 'about', [path])
+	var cvPath = 'cv';
+    var etcPath = 'etc';
+    requestHandler(req, res, 'about', 'About', 'about', [cvPath, etcPath])
 });
 
 app.get('/friends', function (req, res){
-    var path = 'friends?state=published';
-    requestHandler(req, res, 'friends', 'Madi Manson - Friends', 'friends', [path])
+    var friendsPath = 'friends?{"state":"published","$sort":{"weighting":-1}}';
+    var etcPath = 'etc';
+    requestHandler(req, res, 'friends', 'Friends', 'friends', [friendsPath, etcPath])
 });
 
 http.createServer(app).listen(app.get('port'), function(){
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,6 +7,7 @@ $(window).load(function(){
 	});
 
 	alignTitle();
+	// resetThreshold();
 	window.addEventListener('resize', alignTitle);
 
 	function alignTitle(){
@@ -19,24 +20,101 @@ $(window).load(function(){
 			var leftPadding = extraSpace / 2;
 		}
 		$('.title-strip').css('padding-left', leftPadding + 'px');
+		$('.title-strip-prints').css('padding-left', leftPadding + 'px');
+	}
+
+	var currentScrollPx = $(document).scrollTop();
+	var headerCollapsed = false;
+	var threshold = $(document).height() - $(window).height();
+
+	function resetThreshold(){
+		console.log('resetting threshold');
+		headerCollapsed = false;
+		threshold = $(document).height() - $(window).height();
+		console.log('threshold is' + threshold);
+	}
+
+	$(window).on('scroll', function(e){
+		var scrollTop = $(document).scrollTop();
+		if(scrollTop > currentScrollPx){
+			if(scrollTop > 45 && scrollTop < threshold){
+				if(!headerCollapsed){
+					headerCollapsed = true;
+					collapseHeader();
+					// console.log('more than 45' + headerCollapsed);
+				}
+			}
+		}
+		else if($(document).scrollTop() < 55){
+			if(headerCollapsed){
+				headerCollapsed = false;
+				expandHeader();
+				// console.log('less than 45' + headerCollapsed);
+			}
+		}
+		currentScrollPx = scrollTop;
+	});
+
+	function collapseHeader(){
+		var width = document.documentElement.clientWidth;
+		if(width < 480){
+			$('.nav-row').animate({top: "2em"}, 500);
+		}
+		else{
+		// nav and small icon:
+		// $('.nav-row').animate({top: "3em"}, 500);
+		// $('.logo').animate({backgroundSize: '70', height: '5em'}, 500);
+
+		// just small icon:
+		$('.nav-row').animate({top: ".25em"}, 500);
+		$('.logo').animate({backgroundSize: '70', height: '5em'}, 500);
+
+		// just large icon:
+		// $('.nav-row').animate({top: "4em"}, 500);
+		}
+	}
+
+	function expandHeader(){
+		var width = document.documentElement.clientWidth;
+		if(width < 480){
+			$('.nav-row').animate({top: "4.5em"}, 500);
+		}
+		else{
+			$('.nav-row').animate({top: "8.375em"}, 500);
+			$('.logo').animate({backgroundSize: '110', height: '8.75em'}, 500);
+		}
 	}
 
 // !~!~!~!~!!CAROUSEL!~!~!~!~!~!!
 
 	if($('#carousel').data('total-pics') == 1){
 		$("#right-arrow").css('visibility', 'hidden');
+		$("#right-arrow-prints").css('visibility', 'hidden');
 	}
 
 	if($('#carousel').data('total-pics') == $('#carousel').data('visible-index')){
 		$("#right-arrow").css('visibility', 'hidden');
+		$("#right-arrow-prints").css('visibility', 'hidden');
 	}
 
 	if($('#carousel').data('visible-index') != 1){
 		$('#left-arrow').css('visibility', 'visible');
+		$('#left-arrow-prints').css('visibility', 'visible');
 	}
 
 	$('.title-strip').text($('#carousel li:first-child').data('proj-title'));
+	var n = $('#carousel').data('visible-index') || 1;
+	$('.title-strip-prints').text($('#carousel li:nth-child(' + n + ')').data('proj-title'));
 	$('.title-strip').css('visibility', 'visible');
+
+	function convertToSlug(Text)
+	{
+    return Text
+        .toLowerCase()
+        .replace(/[^\w ]+/g,'')
+        .replace(/ +/g,'-')
+        ;
+    }
 
 	$("#right-arrow").click(function(){
 		var arrow = $(this);
@@ -56,7 +134,30 @@ $(window).load(function(){
 			$('.title-strip').text(next.data('proj-title'));
 			window.history.pushState({}, "", "/" + window.location.pathname.split('/')[1] + "/" + window.location.pathname.split('/')[2] + "/" + nextI);
 			next.fadeIn('fast', function(){
+				resetThreshold();
+			});
+		});
+	});
 
+	$("#right-arrow-prints").click(function(){
+		var arrow = $(this);
+		var i = $('#carousel').data('visible-index');
+		var total = $('#carousel').data('total-pics');
+		var nextI = i + 1;
+		var showing = $( "#carousel li:nth-child(" + i + ")" );
+		var next = $( "#carousel li:nth-child(" + nextI + ")" )
+		showing.fadeOut("fast", function(){
+			if(nextI == total){
+				arrow.css('visibility', 'hidden');
+			}
+			if(nextI == 2){
+				$('#left-arrow-prints').css('visibility', 'visible');
+			}
+			$('#carousel').data('visible-index', nextI)
+			$('.title-strip-prints').text(next.data('proj-title'));
+			window.history.pushState({}, "", "/" + window.location.pathname.split('/')[1] + "/" + convertToSlug(next.data('proj-title')));
+			next.fadeIn('fast', function(){
+				resetThreshold();
 			});
 		});
 	});
@@ -77,9 +178,32 @@ $(window).load(function(){
 			}
 			$("#carousel").data('visible-index', prevI);
 			$('.title-strip').text(prev.data('proj-title'));
-			window.history.pushState({}, "", "/textile/" + prevI);
+			window.history.pushState({}, "", "/" + window.location.pathname.split('/')[1] + "/" + window.location.pathname.split('/')[2] + "/" + prevI);
 			prev.fadeIn("fast", function(){
+				resetThreshold();
+			});
+		});
+	});
 
+	$("#left-arrow-prints").click(function(){
+		var arrow = $(this);
+		var i = $('#carousel').data('visible-index');
+		var total = $('#carousel').data('total-pics');
+		var prevI = i - 1;
+		var showing = $("#carousel li:nth-child(" + i + ")");
+		var prev = $("#carousel li:nth-child(" + prevI + ")");
+		showing.fadeOut("fast", function(){
+			if(prevI == total-1){
+				$('#right-arrow-prints').css('visibility', 'visible');
+			}
+			if(prevI == 1){
+				arrow.css('visibility', 'hidden');
+			}
+			$("#carousel").data('visible-index', prevI);
+			$('.title-strip-prints').text(prev.data('proj-title'));
+			window.history.pushState({}, "", "/" + window.location.pathname.split('/')[1] + "/" + convertToSlug(prev.data('proj-title')));
+			prev.fadeIn("fast", function(){
+				resetThreshold();
 			});
 		});
 	});
